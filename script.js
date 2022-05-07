4b3f69c4193f42c50525cab1afeffdce663967db
marcobaron <marcobaron@hotmail.com>
2022-05-06 22:28:59 -0300

    Create the functions for basic operations

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(a, b) { 
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply (a, b) {
+  return a * b;
+}
+
+function divide (a, b) {
+  return a / b;
+}
+
+console.log(add(10, 5));
+console.log(subtract(25, 3));
+console.log(multiply(3, 8));
+console.log(divide(25, 5));
